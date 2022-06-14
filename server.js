22fc0d11bc9f1f222998ac774ca90649ae5b9b2e
Domagoj <domagojpbabic@gmail.com>
2022-06-14 19:40:07 +0200

    final version with comments

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 let express = require('express');
 let cors = require('cors');
-const PORT = 3001;
 const app = express();
+const PORT = 3001;
 
 app.use(cors())
 app.use(express.json())
 
-// To test if server was running ok. 
+// Test if server is running properly.
 app.get('/', (request, response)=>{
     response.json('Pinging back.')
 })
+
+app.get('/info', (request, response)=>{
+  let date = new Date();
+  response.send(`Phonebook bas info for ${phonebook.length} people.
+  ${date}`)
+})
+
 app.get('/api/persons', (request, response) =>{
     response.json(phonebook)
 })
-app.get('/info', (request, response)=>{
-    let date = new Date();
-    response.send(`Phonebook bas info for ${phonebook.length} people.
-    ${date}`)
-})
-// 3.3
+
 app.get('/api/persons/:id', (request, response)=>{
   const id = Number(request.params.id)
   const contact = phonebook.find( ele => ele.id === id)
@@ -29,23 +31,26 @@ app.get('/api/persons/:id', (request, response)=>{
   }
 })
 
-// generate Id used in new entry 
+// generate Id used for new entry.
 function generateId(){
   return phonebook.length > 0 ? Math.max( ...phonebook.map(ele => ele.id)) +1 : 0
 }
 
+// Add new person in phonebook.
 app.post('/api/persons', (request, response)=>{
   let reqBody = request.body;
+  // If entry is missing NAME or NUMBER throws error.
+  console.log(reqBody);
   if(!reqBody.name || !reqBody.number){
-    response.send('Error: missing name and/or number')
+    response.send('Error: missing name and/or number.')
+    // If NAME is already in phonebook throws error.
   }else if(phonebook.find(ele => ele.name == reqBody.name)){
     response.send('Error, name already exists in phonebook.')
   } else {
-
-      let entry = {
-        'id': generateId(),
-        'name': reqBody.name,
-        'number': reqBody.number
+    let entry = {
+      'id': generateId(),
+      'name': reqBody.name,
+      'number': reqBody.number
       }
       phonebook.push(entry)
       response.json(phonebook)
